b78127432e248caf849e47ab3768cd08ada0f083
Gabriele Giardino <gabrielegiardino@hotmail.it>
2026-07-26 17:41:30 +0200

    Fix QBeauty product return hash navigation

--- q_beauty/src/components/ScrollToTop.jsx
+++ q_beauty/src/components/ScrollToTop.jsx
@@ -3,9 +3,45 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
+
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, [pathname]);
+    if (!hash) {
+      window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+      return undefined;
+    }
+
+    const targetId = decodeURIComponent(hash.replace(/^#/, ""));
+
+    const scrollToHashTarget = () => {
+      const target = document.getElementById(targetId);
+      if (!target) {
+        return false;
+      }
+
+      target.scrollIntoView({ behavior: "auto", block: "start" });
+      return true;
+    };
+
+    if (scrollToHashTarget()) {
+      return undefined;
+    }
+
+    const timeoutId = window.setTimeout(() => {
+      scrollToHashTarget();
+    }, 180);
+
+    const rafId = window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(() => {
+        scrollToHashTarget();
+      });
+    });
+
+    return () => {
+      window.clearTimeout(timeoutId);
+      window.cancelAnimationFrame(rafId);
+    };
+  }, [pathname, hash]);
+
   return null;
 }
